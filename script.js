51fbf0414133846333214ef8e8fa8d7649dd1d2c
khyreerusydi <nicoraventbh@gmail.com>
2019-03-26 09:50:14 +0800

    created files for working game

---
+++ script.js
@@ -0,0 +1,176 @@
+// random number generator for 1 - 4
+//Math.floor(Math.random() * 4) + 1;
+
+// default sequence before random generation
+var sequence = [0,0,0,0,0,0,0,0,0,0];
+var sequenceToShow = [];
+var playerSequence = [];
+
+var offset = 0;
+
+var round = 0;
+var best = 0;
+var displayRound = document.getElementById("round");
+var displayBest = document.getElementById("best");
+
+
+var colourCode = {
+    1: "red",
+    2: "blue",
+    3: "green",
+    4: "yellow",
+};
+
+var tile = 4;
+
+
+// sequence generator
+function generateSequence (roundNumber) {
+    for (i=0; i<10; i++) {
+        sequence[i]= Math.floor(Math.random() * 4) + 1;
+    };
+    convert()
+};
+
+// convert generated sequence
+function convert(){
+        for (var i = 0; i < sequence.length; i++) {
+            if (sequence[i] === 1) {
+                sequence[i] = "red";
+                // convertedSequence.push(sequence[i])
+            }
+            else if (sequence[i] === 2) {
+                sequence[i] = "blue";
+                // convertedSequence.push(sequence[i])
+            }
+            else if (sequence[i] === 3) {
+                sequence[i] = "green";
+                // convertedSequence.push(sequence[i])
+            }
+            else if (sequence[i] === 4) {
+                sequence[i] = "yellow";
+                // convertedSequence.push(sequence[i])
+            }
+        }
+    console.log(sequence);
+    // console.log(convertedSequence)
+}
+
+// on load, ready start button
+window.onload = function(){
+    document.getElementById("start").addEventListener('click', startGame);
+};
+
+// when player clicks on start game, generate sequence, bump round counter
+function startGame(){
+    generateSequence();
+    listenToPlayer();
+    newRound();
+}
+
+function newRound(){
+    playerSequence = [];
+    sequenceToShow = [];
+    offset = 0;
+    round++;
+    console.log("round: "+ round)
+    displayRound.innerText = round;
+    showSequence();
+}
+
+// when next round triggers, flash lights based on sequence
+function showSequence(){
+    for (var i = 0; i < round; i++) {
+        sequenceToShow.push(sequence[i])
+    };
+    sequenceToShow.map(displayTile);
+}
+
+function displayTile(value){
+        var lights = document.getElementById(value);
+        // console.log(lights);
+        setTimeout(function(){
+            lights.classList.add("bk");
+        }, (100+offset));
+        setTimeout(function(){
+            lights.classList.remove("bk");
+        }, (600+offset));
+        offset += 600;
+}
+
+// add event listeners for clicks on tiles
+function listenToPlayer(){
+    document.getElementById("red").addEventListener('click', pushPlayerSequence);
+    document.getElementById("blue").addEventListener('click', pushPlayerSequence);
+    document.getElementById("green").addEventListener('click', pushPlayerSequence);
+    document.getElementById("yellow").addEventListener('click', pushPlayerSequence);
+};
+
+// remove event listeners for clicks on tiles
+function removePlayerListener(){
+    document.getElementById("red").removeEventListener('click', pushPlayerSequence);
+    document.getElementById("blue").removeEventListener('click', pushPlayerSequence);
+    document.getElementById("green").removeEventListener('click', pushPlayerSequence);
+    document.getElementById("yellow").removeEventListener('click', pushPlayerSequence);
+};
+
+//add clicks into playerSequence
+function pushPlayerSequence(){
+    playerSequence.push(event.target.id);
+    console.log("player Sequence: " + playerSequence);
+    checkSequence();
+};
+
+//check playerSequence matches sequence
+function checkSequence(){
+    //if player clicked wrong sequence
+    var i = playerSequence.length
+    if (playerSequence[i-1] !== sequence[i-1]) {
+        console.log("wrong")
+        alert("Wrong sequence!\nGame Over!")
+        // if current round is higher than best round, update best round
+        if (round > best) {
+            best = round;
+            displayBest.innerText = (best-1);
+            resetGame();
+        }
+        else {
+            resetGame();
+        }
+        return;
+    }
+    //else player has clicked correct sequence
+    else {
+        console.log("correct");
+        // and if player completed sequence for current round
+        if (playerSequence.length === round) {
+            // and if the game is completed
+            if (round == 10) {
+                alert("game won")
+                // displays current best round
+                if (round > best) {
+                    best = round;
+                    displayBest.innerText = (best-1);
+                    resetGame();
+                }
+                else {
+                    resetGame();
+                }
+            }
+            // else player has completed the round but not the game
+            else {
+            newRound();
+            }
+        }
+    }
+}
+
+// reset game
+function resetGame() {
+    removePlayerListener();
+    sequence = [];
+    playerSequence = [];
+    offset = 0;
+    round = 0;
+    displayRound.innerText = "___"
+}
