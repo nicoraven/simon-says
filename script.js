f78c6e9201fe7829d77e1e2175db99c48fa34041
khyreerusydi <nicoraventbh@gmail.com>
2019-03-27 23:15:52 +0800

    updated accordion menu

--- script.js
+++ script.js
@@ -369,3 +369,15 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
