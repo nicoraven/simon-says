ad5d9ed493bc45e6b4357dd3c797a086f0ea4b2c
khyreerusydi <nicoraventbh@gmail.com>
2019-03-27 22:54:53 +0800

    updated looks

--- script.js
+++ script.js
@@ -333,3 +333,22 @@ function resetGame() {
     showRound.style.visibility = "visible";
     showBest.style.visibility = "visible";
 }
+
+// script for accordion menu
+
+var acc = document.getElementsByClassName("accordion");
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+
+        // Toggle between hiding and showing the active panel
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        }
+        else {
+            panel.style.display = "block";
+        }
+    });
+}
